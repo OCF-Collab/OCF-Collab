8980b69ebc4102550ea19ea2f6015bb83ff0b78a
Nathan Bertram <nbertram@gmail.com>
2020-09-03 23:04:14 -0700

    Auth refinements

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -6,6 +6,6 @@ module Secured
   end
 
   def logged_in_using_omniauth?
-    redirect_to login_path unless session[:userinfo].present? || Authentication.app_authenticated?
+    redirect_to login_path unless Authentication.app_authenticated?
   end
 end
