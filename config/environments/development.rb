ac132fff2ac7a23726796ecf9e9121e9f139263a
Tomasz Werbicki <tomasz@werbicki.net>
2020-10-10 13:45:32 +0200

    Setup external RNA in dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow to be reached by docker-compose service name
+  config.hosts << "web"
 end
