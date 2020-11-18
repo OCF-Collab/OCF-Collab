a718a3e13553b0d66640ab46f3dd8aa4ddda0d5b
Tomasz Werbicki <tomasz@werbicki.net>
2020-11-18 11:40:21 +0100

    Respond to errors as JSON

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,9 @@
 module Api
   class BaseController < ActionController::API
     before_action :doorkeeper_authorize!
+
+    rescue_from StandardError do |exception|
+      render json: { :error => exception.message }, :status => 500
+    end
   end
 end
