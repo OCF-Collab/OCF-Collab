a218ea486373d333fa2d4caa70eec27c2b558d57
Tomasz Werbicki <tomasz@werbicki.net>
2020-11-25 16:24:51 +0100

    Use environment variables for AWS credentials

--- config/initializers/s3_client.rb
+++ config/initializers/s3_client.rb
@@ -1,13 +1,6 @@
-# S3Client = Aws::S3::Client.new(
-#   region: ENV["AWS_REGION"],
-#   access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-#   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-#   stub_responses: Rails.env.test?,
-# )
-
 S3Client = Aws::S3::Client.new(
-  region: "us-east-1",
-  access_key_id: "AKIAS3RXG6HW2T6OBUNE",
-  secret_access_key: "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",
+  region: ENV["AWS_REGION"],
+  access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+  secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
   stub_responses: Rails.env.test?,
 )
