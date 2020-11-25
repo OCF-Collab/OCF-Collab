4180767f72ccc1474e0541f1f451ce45534a0b44
Tomasz Werbicki <tomasz@werbicki.net>
2020-11-25 18:36:04 +0100

    Add empty page at root url

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def empty
+    head :no_content
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: "pages#empty"
+
   use_doorkeeper do
     # it accepts :authorizations, :tokens, :token_info, :applications and :authorized_applications
     skip_controllers :applications, :authorized_applications
