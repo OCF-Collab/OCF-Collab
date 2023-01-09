e8a1245e9f0ca170d7cedb810c9076e131ef9803
Alex Nizamov <nizamov.alex@gmail.com>
2023-01-09 22:54:26 +0500

    Enhance competency mapper to detect data types of converted properties

--- app/services/competency_mapper.rb
+++ app/services/competency_mapper.rb
@@ -11,18 +11,55 @@ class CompetencyMapper
   end
 
   def transformed_body
-    data = properties.map do |property|
+    converted_properties = properties.map do |property|
       value = body[property.fetch("@id")]
       next unless value
 
-      value = value.values.first if value.is_a?(Hash)
-      [property.fetch("rdfs:label"), value]
+      [property.fetch("rdfs:label"), build_value(value)]
     end
 
-    { metadata:, properties: data.compact.to_h }.to_json
+    { metadata:, properties: converted_properties.compact.to_h }.to_json
   end
 
   private
+
+  def build_value(value)
+    return value.map { |v| build_value(v) } if value.is_a?(Array)
+    return build_value(value.values.first) if value.is_a?(Hash)
+
+    type = detect_type(value)
+
+    metadata =
+      if type == "uri"
+        { resource_type: detect_resource_type(value) }
+      else
+        {}
+      end
+
+    {
+      type: detect_type(value),
+      value:,
+      **metadata
+    }
+  end
+
+  def detect_resource_type(value)
+    return "competency" if Competency.exists?(external_id: value)
+    return "container" if CompetencyFramework.exists?(external_id: value)
+  end
+
+  def detect_type(value)
+    return "uri" if valid_uri?(value)
+
+    "string"
+  end
+
+  def convert_property(property)
+    value = body[property.fetch("@id")]
+    return unless value
+
+    [property.fetch("rdfs:label"), build_value(value)]
+  end
 
   def mapping
     filename = "#{node_directory.s3_bucket}.json"
@@ -42,5 +79,12 @@ class CompetencyMapper
       resource.fetch("@type") == "rdf:Property" &&
         resource.dig("rdfs:domain", "@id") == "ceasn:Competency"
     end
+  end
+
+  def valid_uri?(value)
+    uri = URI.parse(value)
+    uri.is_a?(URI::HTTP) && uri.scheme.starts_with?("http")
+  rescue URI::InvalidURIError
+    false
   end
 end
