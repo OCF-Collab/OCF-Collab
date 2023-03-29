4fceb78b36009bcfb86f920dfefbb84339ad9027
Alex Nizamov <nizamov.alex@gmail.com>
2023-03-29 13:18:17 +0500

    Disable retries in node entry sync job

--- app/jobs/node_directory_entry_sync_job.rb
+++ app/jobs/node_directory_entry_sync_job.rb
@@ -1,6 +1,8 @@
 class NodeDirectoryEntrySyncJob < ApplicationJob
   queue_as :default
 
+  sidekiq_options retry: 0
+
   def perform(node_directory:, s3_key:)
     NodeDirectoryEntrySync.new(
       node_directory: node_directory,
