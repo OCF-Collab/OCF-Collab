b2f4178a55e748749c051c792618a8985ba4c940
Alex Nizamov <nizamov.alex@gmail.com>
2023-04-03 13:15:29 +0500

    Simplify ES queries

--- app/services/search.rb
+++ app/services/search.rb
@@ -61,24 +61,41 @@ class Search
   end
 
   def containers
-    @containers ||= Competency
-      .search(
-        body: {
-          aggs: {
-            containers: {
-              terms: {
-                field: :container_external_id,
-                size: MAX_SIZE
-              }
+    @containers ||= begin
+      container_query =
+        if container_type
+          {
+            bool: {
+              must: [
+                { term: { container_type: } },
+                *query.dig(:bool, :must)
+              ],
+              should: query.dig(:bool, :should)
             }
+          }
+        else
+          query
+        end
+
+      Competency
+        .search(
+          body: {
+            aggs: {
+              containers: {
+                terms: {
+                  field: :container_external_id,
+                  size: MAX_SIZE
+                }
+              }
+            },
+            query: container_query,
+            size: 0
           },
-          query:,
-          size: 0
-        },
-        per_page: MAX_SIZE
-      )
-      .aggs
-      .dig("containers", "buckets")
+          per_page: MAX_SIZE
+        )
+        .aggs
+        .dig("containers", "buckets")
+    end
   end
 
   def page
@@ -94,12 +111,10 @@ class Search
   def query
     @query ||= begin
       optional, required = facets.partition { |f| f[:optional] }
-      required_conditions = required.map { |f| build_condition(f) }
-      required_conditions << { match: { container_type: } } if container_type
 
       {
         bool: {
-          must: required_conditions,
+          must: required.map { |f| build_condition(f) },
           should: optional.map { |f| build_condition(f) }
         }
       }
