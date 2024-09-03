e944003204df5c8d6935ca2d0d6708911f7dfbf7
Alex Nizamov <nizamov.alex@gmail.com>
2024-09-03 10:43:04 +0500

    Catch errors during competency import

--- app/services/node_directory_entry_sync.rb
+++ app/services/node_directory_entry_sync.rb
@@ -8,14 +8,125 @@ class NodeDirectoryEntrySync
   end
 
   def sync!
-    update_contextualizing_objects!
-    update_container!
+    Container.transaction do
+      update_contextualizing_objects!
+      update_container!
+      update_competencies!
+    end
+
     reindex
   rescue Aws::S3::Errors::NoSuchKey
     delete_existing_container!
   end
 
   private
+
+  def container
+    @container ||= node_directory.
+      containers.
+      find_or_initialize_by(node_directory_s3_key: s3_key)
+  end
+
+  def container_attributes
+    {
+      external_id: parsed_container[:url],
+      type: parsed_container[:type],
+      name: parsed_container[:name],
+      description: parsed_container[:description],
+      concept_keywords: parsed_container[:concept_keywords],
+      attribution_name: parsed_container[:attribution_name],
+      attribution_url: parsed_container[:attribution_url],
+      provider_meta_model: parsed_container[:provider_meta_model],
+      beneficiary_rights: parsed_container[:beneficiary_rights],
+      registry_rights: parsed_container[:registry_rights],
+      data_url: parsed_container[:data_url]
+    }
+  end
+
+  def delete_existing_container!
+    Container.find_by(node_directory_s3_key: s3_key)&.delete
+  end
+
+  def entry_data
+    @entry_data ||= JSON.parse(s3_object_response_body)
+  end
+
+  def parsed_container
+    @parsed_container ||= NodeDirectoryEntryParser.new(
+      entry_data: entry_data,
+    ).parsed_container
+  end
+
+  def reindex
+    container.competencies.includes(contextualizing_objects: :codes).reindex
+  end
+
+  def s3_bucket
+    node_directory.s3_bucket
+  end
+
+  def s3_client
+    @s3_client ||= S3Client.new(node_directory:)
+  end
+
+  def s3_object_response
+    @s3_object_response ||= s3_client.get_object(
+      bucket: s3_bucket,
+      key: s3_key,
+    )
+  end
+
+  def s3_object_response_body
+    @s3_object_resoinse_body = s3_object_response.body.read
+  end
+
+  def update_codes!(parsed_categories)
+    parsed_categories.map do |parsed_category|
+      code_set = CodeSet
+        .create_with(name: parsed_category[:in_code_set])
+        .find_or_create_by!(external_id: parsed_category[:in_code_set])
+
+      code_set
+        .codes
+        .create_with(
+          description: parsed_category[:description],
+          name: parsed_category[:name] || ""
+        )
+        .find_or_create_by!(value: parsed_category[:code_value])
+    end
+  end
+
+  def update_competencies!
+    container.competencies.delete_all
+
+    parsed_container[:competencies].map do |parsed_competency|
+      contextualizing_objects = ContextualizingObject.where(
+        external_id: Array.wrap(parsed_competency[:contextualized_by])
+      )
+
+      external_id = parsed_competency[:id]
+
+      begin
+        container
+          .competencies
+          .create!(
+            comment: parsed_competency[:comment],
+            competency_category: parsed_competency[:competency_category],
+            competency_label: parsed_competency[:competency_label],
+            competency_text: parsed_competency[:competency_text],
+            contextualizing_objects:,
+            external_id:,
+            keywords: parsed_competency[:keywords]
+          )
+      rescue => e
+        Airbrake.notify(
+          e,
+          competency_id: external_id,
+          container_id: container.id
+        )
+      end
+    end
+  end
 
   def update_container!
     container.update!(container_attributes)
@@ -37,99 +148,5 @@ class NodeDirectoryEntrySync
         )
         .find_or_create_by!(external_id:)
     end
-  end
-
-  def update_codes!(parsed_categories)
-    parsed_categories.map do |parsed_category|
-      code_set = CodeSet
-        .create_with(name: parsed_category[:in_code_set])
-        .find_or_create_by!(external_id: parsed_category[:in_code_set])
-
-      code_set
-        .codes
-        .create_with(
-          description: parsed_category[:description],
-          name: parsed_category[:name] || ""
-        )
-        .find_or_create_by!(value: parsed_category[:code_value])
-    end
-  end
-
-  def container
-    @container ||= Container.find_or_initialize_by(node_directory_s3_key: s3_key) do |container|
-      container.node_directory = node_directory
-    end
-  end
-
-  def parsed_container
-    @parsed_container ||= NodeDirectoryEntryParser.new(
-      entry_data: entry_data,
-    ).parsed_container
-  end
-
-  def entry_data
-    @entry_data ||= JSON.parse(s3_object_response_body)
-  end
-
-  def s3_object_response_body
-    @s3_object_resoinse_body = s3_object_response.body.read
-  end
-
-  def s3_object_response
-    @s3_object_response ||= s3_client.get_object(
-      bucket: s3_bucket,
-      key: s3_key,
-    )
-  end
-
-  def s3_bucket
-    node_directory.s3_bucket
-  end
-
-  def s3_client
-    @s3_client ||= S3Client.new(node_directory:)
-  end
-
-  def container_attributes
-    {
-      external_id: parsed_container[:url],
-      type: parsed_container[:type],
-      name: parsed_container[:name],
-      description: parsed_container[:description],
-      concept_keywords: parsed_container[:concept_keywords],
-      attribution_name: parsed_container[:attribution_name],
-      attribution_url: parsed_container[:attribution_url],
-      provider_meta_model: parsed_container[:provider_meta_model],
-      beneficiary_rights: parsed_container[:beneficiary_rights],
-      registry_rights: parsed_container[:registry_rights],
-      competencies:,
-      data_url: parsed_container[:data_url]
-    }
-  end
-
-  def competencies
-    parsed_container[:competencies].map do |parsed_competency|
-      contextualizing_objects = ContextualizingObject.where(
-        external_id: Array.wrap(parsed_competency[:contextualized_by])
-      )
-
-      Competency.new(
-        competency_category: parsed_competency[:competency_category],
-        competency_label: parsed_competency[:competency_label],
-        competency_text: parsed_competency[:competency_text],
-        comment: parsed_competency[:comment],
-        contextualizing_objects:,
-        external_id: parsed_competency[:id],
-        keywords: parsed_competency[:keywords]
-      )
-    end
-  end
-
-  def delete_existing_container!
-    Container.find_by(node_directory_s3_key: s3_key)&.delete
-  end
-
-  def reindex
-    container.competencies.includes(contextualizing_objects: :codes).reindex
   end
 end
